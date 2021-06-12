65ae12e1d4130006903337d16471c5c725c746e9
otav-o <otaviocacadordioscanio@gmail.com>
2021-06-12 10:26:08 -0300

    Finalizar questão 1

--- AtividadesAvaliativas/SegundoBimestre/Prova/script.js
+++ AtividadesAvaliativas/SegundoBimestre/Prova/script.js
@@ -6,8 +6,23 @@ const inicio = () => {
   });
   document
     .getElementById("botao-adivinhar")
-    .setAttribute("onclick", "adivinharNumero()");
+    .setAttribute("onclick", "adivinharNumero(0, 100)");
 };
+
+const adivinharNumero = (primeiro, ultimo) => {
+  let palpite = prompt(`Digite um número entre ${primeiro} e ${ultimo}.`);
+  let numeroComputador = sortearNumero(primeiro, ultimo);
+  let operador;
+
+  if (palpite > numeroComputador) operador = "maior que";
+  else if (palpite < numeroComputador) operador = "menor que";
+  else operador = "igual ao";
+
+  alert(`Seu número ${palpite} é ${operador} meu número ${numeroComputador}`);
+};
+
+const sortearNumero = (primeiro, ultimo) =>
+  Math.floor(Math.random() * (ultimo - primeiro + 1)) + primeiro;
 
 /** Função que cria uma tag HTML e adiciona ao DOM */
 const gerarElemento = ({ tag, id, idPai, classe, conteudo }) => {
